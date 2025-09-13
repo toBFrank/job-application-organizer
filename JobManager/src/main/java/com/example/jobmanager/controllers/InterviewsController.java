0372133e7e79d9682f9e6b3f40a14ff1a52d1380
Elaiza Remo <elaiza@Elaizas-MacBook-Air-7.local>
2025-09-13 12:49:15 -0600

    controller update

--- JobManager/src/main/java/com/example/jobmanager/controllers/InterviewsController.java
+++ JobManager/src/main/java/com/example/jobmanager/controllers/InterviewsController.java
@@ -1,4 +1,4 @@
-package com.example.jobmanager.controllers;
+package com.example.jobmanager.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
