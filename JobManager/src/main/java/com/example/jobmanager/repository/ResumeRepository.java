42c9d05709529c9ce288cb1e075216980aaeef40
Diego Arana <diegoarana@MacBook-Pro-164.local>
2025-09-13 13:43:18 -0600

    bug-fix: added missing import

--- JobManager/src/main/java/com/example/jobmanager/repository/ResumeRepository.java
+++ JobManager/src/main/java/com/example/jobmanager/repository/ResumeRepository.java
@@ -3,6 +3,9 @@ package com.example.jobmanager.repository;
 import com.example.jobmanager.entity.Resume;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ResumeRepository extends JpaRepository<Resume, Long> {
     // find master resume by its ID
     List<Resume> findByMasterResumeId(Long masterResumeId);
