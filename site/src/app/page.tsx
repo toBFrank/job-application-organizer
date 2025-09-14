66966c33ae67bd6152db92b913df2afc0f109e48
Peter Thai <51303643+pwthai@users.noreply.github.com>
2025-09-14 11:13:30 -0600

    Feature/create landing page (#24)
    * Add more comments on readme
    
    * Add watcher to front end and back end
    
    * temp header + nav
    
    * landing page with header + table
    
    * application drag and drop table
    
    * Small fixups
    
    * Fix docker-compose
    
    * Temp fix
    
    * Fix header file name casing
    
    * Fix spacing
    
    * Yippeee! drag and drop
    
    ---------
    
    Co-authored-by: echiewe <126719218+echiewe@users.noreply.github.com>

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -1,19 +1,29 @@
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center">
-        <div id="kanban">
-          <div id="kanban-header" className="flex">
-            <div className="border-2 border-amber-300 p-2">Applied</div>
-            <div className="border-2 border-amber-300 p-2">Interviewing</div>
-            <div className="border-2 border-amber-300 p-2">Job Offer</div>
-            <div className="border-2 border-amber-300 p-2">Rejected</div>
-          </div>
-          <div id="kanban-data">
+import Job_Board from './components/JobBoard'
 
-          </div>
-        </div>        
-      </main>
-    </div>
+export default function Home() {
+  return (    
+    <main className="">
+      <div className="flex items-center border-b border-gray-400 shadow-sm justify-between bg-gray-50 pb-5 pt-6 px-10">
+        <h1 className="text-2xl md:text-3xl font-semibold">Your Job Applications</h1>
+        <div className="flex items-center gap-2">
+          <input 
+            placeholder='Job title...'
+            className="px-3 py-2 border rounded-md bg-white w-64"
+          />
+          <input
+            placeholder="Company name..."
+            className="px-3 py-2 border rounded-md bg-white w-64"
+          />
+          <button className="px-4 py-2 bg-teal-500 text-white hover:bg-white hover:text-teal-500 rounded-md">
+            Add Applications
+          </button>
+        </div>
+      </div>
+
+      <div className='pt-5 px-10'>
+        <Job_Board />
+      </div>
+
+    </main>
   );
 }
