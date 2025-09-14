53a3b728cf3bfd8711f939a69ee24f1c04d04f28
Peter Thai <pwht.107@gmail.com>
2025-09-14 10:47:54 -0600

    Fix spacing

--- site/src/app/components/Header.tsx
+++ site/src/app/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
     return (
     <nav className="w-full border-b border-gray-400 bg-white shadow-sm sticky top-0 z-50">
-        <div className="container mx-auto px-10 py-3">
+        <div className="w-full px-10 py-3">
             <div id='nav' className="flex flex-wrap justify-between min-w-0 gap-4">
                 <div className="md:flex items-center space-x-8">
                     <Link href='/' className="hover:text-teal-500 transition-colors duration-300">Dashboard</Link>
